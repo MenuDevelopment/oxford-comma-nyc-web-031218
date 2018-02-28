af6c014acc3dab85883235e9ea13f3578a404c7a
github username <github email address>
2018-02-27 21:56:52 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    sentence = ""
+    for i in 0...array.size
+      if i == array.size - 2
+        sentence+= "#{array[i]}, and "
+      elsif i == array.size - 1
+        sentence += "#{array[i]}"
+      else
+        sentence += "#{array[i]}, "
+      end
+    end
+    sentence
+  end
 end
